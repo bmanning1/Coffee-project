508cc7e88c145293a15aefc986a4d753094c63fb
bmanning1 <rebm01@hotmail.co.uk>
2017-04-24 10:29:02 +0100

    Intitial commit

---
+++ Coffee_project/urls.py
@@ -0,0 +1,69 @@
+"""Coffee_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.conf import settings
+from django.contrib import admin
+from home import views
+from accounts.views import register, profile, login, logout
+from paypal.standard.ipn import urls as paypal_urls
+from paypal_store import views as paypal_views
+from coffees import views as coffee_views
+from django.views.static import serve
+from settings.dev import MEDIA_ROOT
+from threads import views as forum_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.get_index),
+    url(r'^about/$', views.get_about),
+    url(r'^contact/$', views.get_contact),
+    # url(r'^prices/$', views.get_price),
+
+    # Auth URLs
+    # url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^register/$', register, name='register'),
+    url(r'^profile/$', profile, name='profile'),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+
+    # Paypal URLs
+    url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
+    url(r'^paypal-return/$', paypal_views.paypal_return),
+    url(r'^paypal-cancel/$', paypal_views.paypal_cancel),
+    url(r'^coffees/$', coffee_views.all_coffees),
+    url(r'^prices/$', coffee_views.get_price),
+
+    # Blog URLs
+    url(r'^blog/', include('reusable_blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+
+    # Forum URLs
+    url(r'^forum/$', forum_views.forum),
+    url(r'^threads/(?P<subject_id>\d+)/$', forum_views.threads, name='threads'),
+    url(r'^new_thread/(?P<subject_id>\d+)/$', forum_views.new_thread, name='new_thread'),
+    url(r'^thread/(?P<thread_id>\d+)/$', forum_views.thread, name='thread'),
+    url(r'^post/new/(?P<thread_id>\d+)/$', forum_views.new_post, name='new_post'),
+    url(r'^post/edit/(?P<thread_id>\d+)/(?P<post_id>\d+)/$', forum_views.edit_post, name='edit_post'),
+    url(r'^post/delete/(?P<thread_id>\d+)/(?P<post_id>\d+)/$', forum_views.delete_post, name='delete_post'),
+    url(r'^thread/vote/(?P<thread_id>\d+)/(?P<subject_id>\d+)/$', forum_views.thread_vote, name='cast_vote'),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        url(r'^debug/', include(debug_toolbar.urls)),
+    ]
