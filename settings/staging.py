4bf362dc82c28b92bbe9720e3d07d34144fec221
bmanning1 <rebm01@hotmail.co.uk>
2017-05-03 17:47:08 +0100

    heroku

--- settings/staging.py
+++ settings/staging.py
@@ -1,4 +1,5 @@
 from base import *
+import dj_database_url
 
 DEBUG = True
 
@@ -8,6 +9,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] = dj_database_url.config("mysql://b4e94acd0b2f51:eb48feb4@eu-cdbr-west-01.cleardb.com/heroku_cc39facad1a3501?")
 
 # Paypal environment variables
 SITE_URL = 'starbex.herokuapp.com'
