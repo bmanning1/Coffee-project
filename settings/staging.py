b6219b3a12e4d2bd3127c7b1fd37f8ed1d4e75e9
bmanning1 <rebm01@hotmail.co.uk>
2017-05-04 16:19:28 +0100

    heroku

--- settings/staging.py
+++ settings/staging.py
@@ -5,10 +5,14 @@ DEBUG = True
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-DATABASES['default'] = dj_database_url.config("mysql://b4e94acd0b2f51:eb48feb4@eu-cdbr-west-01.cleardb.com/heroku_cc39facad1a3501?")
+CLEAR_DB_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
+
+DATABASES['default'] = dj_database_url.parse(CLEAR_DB_URL)
 
 SITE_URL = 'starbex.herokuapp.com'
 
