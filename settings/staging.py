d795d11a736a48dd107b5db04fb3d2749c4088d6
bmanning1 <rebm01@hotmail.co.uk>
2017-05-04 16:14:25 +0100

    heroku

--- settings/staging.py
+++ settings/staging.py
@@ -5,13 +5,13 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 DATABASES['default'] = dj_database_url.config("mysql://b4e94acd0b2f51:eb48feb4@eu-cdbr-west-01.cleardb.com/heroku_cc39facad1a3501?")
 
-# Paypal environment variables
 SITE_URL = 'starbex.herokuapp.com'
+
+# Paypal environment variables
 PAYPAL_NOTIFY_URL = 'starbex.herokuapp.com/a-very-hard-to-guess-url/'
 PAYPAL_RECEIVER_EMAIL = 'rebm01-facilitator@hotmail.co.uk'
