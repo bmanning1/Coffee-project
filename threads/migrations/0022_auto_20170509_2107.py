df1ed693866ffadd381a4a6e1d8940ac0254573a
bmanning1 <rebm01@hotmail.co.uk>
2017-05-09 21:08:19 +0100

    migrations

---
+++ threads/migrations/0022_auto_20170509_2107.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('threads', '0021_auto_20170506_0000'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='created_at',
+            field=models.DateTimeField(default=datetime.datetime(2017, 5, 9, 20, 7, 0, 311000, tzinfo=utc)),
+        ),
+        migrations.AlterField(
+            model_name='thread',
+            name='created_at',
+            field=models.DateTimeField(default=datetime.datetime(2017, 5, 9, 20, 7, 0, 309000, tzinfo=utc)),
+        ),
+    ]
